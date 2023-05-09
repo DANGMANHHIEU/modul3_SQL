0bc4860996ef12aa6fbd0b1d66b90a901aa60aab
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-05-09 13:38:55 +0700

    truy van du lieu

--- thuc hanh mark.sql
+++ thuc hanh mark.sql
@@ -52,3 +52,27 @@ select s.studentID,s.studentName, avg(mark)
 from student s join mark m on s.studentID = m.studentID
 group by studentID, studentName
 having avg(mark) >= all (select avg(mark) from mark group by mark.studentID);
+
+-- hiển thị thông tin môn học có credit lớn nhất --
+set @max = (select max(credit) from subject);
+select subName,credit
+from subject
+group by subName,credit5
+having credit= @max;
+
+-- hiển thị các thông tin môn học có điểm thi lớn nhất --
+
+set @max= (select max(mark) from mark);
+
+select  s.studentName, c.className, sub.subName, m.mark
+from class c join student s on c.classID = s.classID join mark m on s.studentID = m.studentID join subject sub on m.subId = sub.subID
+group by sub.subname, s.studentName, c.className, m.mark
+having mark = @max;
+
+-- hiển thị thông tin sinh viên và điểm trung bình mỗi sinh viên, xếp hạng theo thứ tự giảm dần --
+
+select s.studentID, s.studentName, c.className, sub.subName,avg(mark) as tb
+from class c join student s on c.classID = s.classID join mark m on s.studentID = m.studentID join subject sub on m.subId = sub.subID
+group by sub.subname, s.studentID
+having avg(mark) 
+order by tb desc;
